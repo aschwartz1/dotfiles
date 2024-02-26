eac8864051ef42eb1195c7ab4087af827b1f69d8
Alex Schwartz <aschwartz@detaso.com>
2024-02-26 13:46:18 -0600

    Add initial lua dir structure

---
+++ nvim/lua/alex/lazy.lua
@@ -0,0 +1,20 @@
+-- Setup lazy as the package manager
+-- https://github.com/folke/lazy.nvim?tab=readme-ov-file#-installation
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- Add lazy.nvim
+-- From the docs, this is: `require("lazy").setup(plugins, opts)`
+require("lazy").setup({})
