ed40a2212e3c69978a5f959306277dbedfee7196
Alex Schwartz <aschwartz@detaso.com>
2024-02-27 08:08:26 -0600

    Add vim-tmux-navigator

---
+++ nvim/lua/alex/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,33 @@
+-- Seamless navigation between tmux panes and vim splits
+return {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    {
+      "<c-h>",
+      "<cmd><C-U>TmuxNavigateLeft<cr>",
+      desc = "Navigate left between tmux/nvim splits"
+    },
+    {
+      "<c-j>",
+      "<cmd><C-U>TmuxNavigateDown<cr>",
+      desc = "Navigate down between tmux/nvim splits"
+    },
+    {
+      "<c-k>",
+      "<cmd><C-U>TmuxNavigateUp<cr>",
+      desc = "Navigate up between tmux/nvim splits"
+    },
+    {
+      "<c-l>",
+      "<cmd><C-U>TmuxNavigateRight<cr>",
+      desc = "Navigate right between tmux/nvim splits"
+    },
+  },
+}
