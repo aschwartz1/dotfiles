18df359287caf69d63253e52c786c05d31eb7a7f
Alex Schwartz <aschwartz@detaso.com>
2024-02-27 14:25:15 -0600

    Add nvim-lint

---
+++ nvim/lua/alex/plugins/nvim-lint.lua
@@ -0,0 +1,25 @@
+-- An asynchronous linter plugin for Neovim complementary to the built-in Language Server Protocol support.
+-- Info source: https://youtu.be/ybUE4D80XSk
+return {
+  "mfussenegger/nvim-lint",
+  event = {
+    "BufReadPre",
+    "BufNewFile",
+  },
+  config = function()
+    local lint = require("lint")
+
+    lint.linters_by_ft = {
+      eruby = { "erb_lint" },
+    }
+
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+  end
+}
